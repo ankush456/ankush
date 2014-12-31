21fd40d29804f8cbdfc96e2ad8719151242d8659
Ankush Verma <ankush@wittyparrot.com>
2014-12-31 13:24:56 +0530

    new files added

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ankushApp
+ * @description
+ * # ankushApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ankushApp', [
+    'ngAnimate',
+    'ngSanitize',
+    'ngTouch'
+  ]);
